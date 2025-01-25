222fc9bbeb31c3951bb726cf5d11c685898fee5a
bindutata <133663572+bindutata@users.noreply.github.com>
2025-01-25 23:41:09 +0530

    routing is modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route,Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Products from "./Components/Products";
 import CartPage from "./Components/CartPage";
-import {ProductsList} from "./Components/ProductsList";
+
 
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       <Products>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<ProductsList/>}></Route>
+          <Route path="/" element={<Products/>}></Route>
           <Route path="/CartPage" element={<CartPage/>}></Route>
         </Routes>
       </Products>
